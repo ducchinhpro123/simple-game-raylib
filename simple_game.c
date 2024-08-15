200397124cc67a2542d6d38074b85ce73b5034a4
ducchinhpro123 <chinh99562@donga.edu.vn>
2024-08-15 16:23:38 +0700

    fix RESTITUTION and GRAVITY

--- simple_game.c
+++ simple_game.c
@@ -12,10 +12,10 @@
 #define RECTANGLE_WIDTH WINDOW_WIDTH * 0.2
 #define RECTANGLE_HEIGHT WINDOW_HEIGHT * 0.05
 
-#define RESTITUTION 0.8
+#define RESTITUTION 0.6f
 #define GRAVITY 980.0f
 #define VELOCITY_THRESOLD 0.5
-#define FIXED_TIME_STEP 1.0f / 90.0f
+#define FIXED_TIME_STEP 1.0f / 60.0f
 #define VELOCITY_THRESHOLD 5.0f
 #define POSITION_THRESHOLD 0.1f
 
@@ -24,11 +24,9 @@
 
 #define FPS 60
 
-int is_on_the_ground(float real_ball_y)
-{
-    if (real_ball_y == WINDOW_HEIGHT) return 1;  // on the ground
-    return 0;
-}
+int is_on_the_ground(float real_ball_y);
+void accumulator_loop(float *accumulator, float *ball_velocity, float *ps_y, float *ps_x);
+void ball_jumb(float *ball_velocity);
 
 int main()
 {
@@ -43,116 +41,8 @@ int main()
 
     while (!WindowShouldClose()) {
         BeginDrawing();
-        /*
-         *          _
-         *   * -> ( * )
-         *          -
-         *   * means position x or y, so ps_y + BALL_SIZE would be a real ball
-         * with vertical only
-         * */
-        float real_ball_y = ps_y + BALL_SIZE;
-        float real_ball_x = ps_x + BALL_SIZE;
 
-        float delta_time = GetFrameTime();
-        accumulator += delta_time;
-
-        while (accumulator > FIXED_TIME_STEP) {
-            ball_velocity += GRAVITY * FIXED_TIME_STEP;
-            ps_y += ball_velocity * FIXED_TIME_STEP;
-
-            // if the ball hit the bottom of the window
-            if (real_ball_y > WINDOW_HEIGHT) {
-                ps_y = WINDOW_HEIGHT - BALL_SIZE;  // ensure the ball hits the bottom of the screen
-                ball_velocity *= RESTITUTION;
-
-                // if velocity is below threshold, stop the ball. Prevent always boucing
-                if (fabs(ball_velocity) > VELOCITY_THRESHOLD) {
-                    // printf("TESTING\n");
-                    ball_velocity *= -RESTITUTION;
-                } else {
-                    ball_velocity = 0;
-
-                    // ensure the ball rest in underground
-                    if ((WINDOW_HEIGHT - real_ball_y) > POSITION_THRESHOLD) {
-                        ps_y = WINDOW_HEIGHT - BALL_SIZE;
-                    }
-                }
-            }
-
-            Vector2 ball_position = {ps_x, ps_y};
-            Rectangle rectangle = {RECTANGLE_X, RECTANGLE_Y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT};
-
-            if (CheckCollisionCircleRec(ball_position, BALL_SIZE, rectangle)) {
-                ps_y = WINDOW_HEIGHT - BALL_SIZE;  // ensure the ball hits the bottom of the screen
-                ball_velocity *= RESTITUTION;
-
-                // if velocity is below threshold, stop the ball. Prevent always boucing
-                if (fabs(ball_velocity) > VELOCITY_THRESHOLD) {
-                    // printf("TESTING\n");
-                    ball_velocity *= -RESTITUTION;
-                } else {
-                    ball_velocity = 0;
-
-                    // ensure the ball rest in underground
-                    if ((WINDOW_HEIGHT - real_ball_y) > POSITION_THRESHOLD) {
-                        ps_y = WINDOW_HEIGHT - BALL_SIZE;
-                    }
-
-                    if (IsKeyPressed(KEY_SPACE)) {
-                        ball_velocity = -20;
-                    }
-                }
-                // ball_velocity = 0.0f;
-            }
-
-            if (IsKeyPressed(KEY_SPACE)) {
-                ball_velocity = -600;
-            }
-            // if (is_on_the_ground(real_ball_y)) {
-            //     ball_velocity = 0;
-            //     if (IsKeyPressed(KEY_SPACE)) {
-            //         ball_velocity = -20;
-            //     }
-            // }
-
-            if (IsKeyDown(KEY_RIGHT) && real_ball_x < WINDOW_WIDTH) {
-                ps_x += 10;
-            }
-
-            if (IsKeyDown(KEY_LEFT) && real_ball_x > (BALL_SIZE * 2)) {
-                ps_x -= 10;
-            }
-
-            accumulator -= FIXED_TIME_STEP;
-        }
-
-        // we can only jumb while we're underground
-        // if (is_on_the_ground(real_ball_y)) {
-        //     ball_velocity = 0;
-        //     if (IsKeyPressed(KEY_SPACE)) {
-        //         ball_velocity = -20;
-        //     }
-        // }
-        // if (IsKeyDown(KEY_RIGHT) && real_ball_x < WINDOW_WIDTH) {
-        //     ps_x += 10;
-        // }
-        //
-        // if (IsKeyDown(KEY_LEFT) && real_ball_x > (BALL_SIZE * 2)) {
-        //     ps_x -= 10;
-        // }
-        //
-        // Vector2 ball_position = {ps_x, ps_y};
-        // Rectangle rectangle = {RECTANGLE_X, RECTANGLE_Y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT};
-        //
-        // if (CheckCollisionCircleRec(ball_position, BALL_SIZE, rectangle)) {
-        //     ball_velocity = 0;
-        //     if (IsKeyPressed(KEY_SPACE)) {
-        //         ball_velocity = -20;
-        //     }
-        // }
-
-        // ps_y += ball_velocity;
-        // ball_velocity += 1.0f;
+        accumulator_loop(&accumulator, &ball_velocity, &ps_y, &ps_x);
 
         ClearBackground(BLACK);
         DrawCircle((int)ps_x, (int)ps_y, BALL_SIZE, RED);
@@ -166,4 +56,106 @@ int main()
     // Set background color
     CloseWindow();
     return 0;
+}
+
+void accumulator_loop(float *accumulator, float *ball_velocity, float *ps_y, float *ps_x)
+{
+    /*
+     *          _
+     *   * -> ( * )
+     *          -
+     *   * means position x or y, so ps_y + BALL_SIZE would be a real ball
+     * with vertical only
+     * */
+    float real_ball_y = *ps_y + BALL_SIZE;
+    float real_ball_x = *ps_x + BALL_SIZE;
+    float delta_time = GetFrameTime();
+    *accumulator += delta_time;
+
+    while (*accumulator >= FIXED_TIME_STEP) {
+        *ball_velocity += GRAVITY * FIXED_TIME_STEP;
+        *ps_y += *ball_velocity * 0.2f;
+        // *ps_y += *ball_velocity * FIXED_TIME_STEP;
+
+        // if the ball hit the bottom of the window
+        if (*ps_y + BALL_SIZE > WINDOW_HEIGHT) {
+            *ps_y = WINDOW_HEIGHT - BALL_SIZE;  // ensure the ball hits the bottom of the screen
+            // ball_velocity *= -RESTITUTION;
+
+            // if velocity is below threshold, stop the ball. Prevent always boucing
+            if (fabs(*ball_velocity) > VELOCITY_THRESHOLD) {
+                *ball_velocity *= -RESTITUTION;  // apply restitution
+            } else {
+                *ball_velocity = 0;  // rest on the ground
+
+                if (WINDOW_HEIGHT - (*ps_y + BALL_SIZE) < POSITION_THRESHOLD) {
+                    *ps_y = WINDOW_HEIGHT - BALL_SIZE;
+                }
+            }
+        }
+
+        Vector2 ball_position = {*ps_x, *ps_y};
+        Rectangle rectangle = {RECTANGLE_X, RECTANGLE_Y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT};
+
+        // check if the ball hits the rectangle
+        if (CheckCollisionCircleRec(ball_position, BALL_SIZE, rectangle)) {
+            *ps_y = RECTANGLE_Y - BALL_SIZE;  // position the ball on the rectangle
+            // *ball_velocity *= -RESTITUTION;   // apply restitution
+
+            // if velocity is below threshold, stop the ball. Prevent always boucing
+            if (fabs(*ball_velocity) > VELOCITY_THRESHOLD) {
+                // printf("TESTING\n");
+                *ball_velocity *= -RESTITUTION;  // apply restitution
+            }
+
+            if ((RECTANGLE_Y - real_ball_y) > POSITION_THRESHOLD) {
+                // *ball_velocity *= -RESTITUTION;  // apply restitution
+                *ps_y = RECTANGLE_Y - BALL_SIZE;
+            } else {
+                *ball_velocity = 0;
+
+                // if (RECTANGLE_HEIGHT - (*ps_y + BALL_SIZE) < POSITION_THRESHOLD) {
+                //     *ps_y = RECTANGLE_HEIGHT - BALL_SIZE;
+                // }
+            }
+
+            if (IsKeyPressed(KEY_SPACE)) {
+                ball_jumb(ball_velocity);
+            }
+        }
+
+        // if (is_on_the_ground(real_ball_y)) {
+        //     ball_velocity = 0;
+        //     if (IsKeyPressed(KEY_SPACE)) {
+        //         ball_velocity = -20;
+        //     }
+        // }
+
+        if (IsKeyPressed(KEY_SPACE)) {
+            // *ball_velocity = -200;
+            ball_jumb(ball_velocity);
+        }
+        if (IsKeyDown(KEY_RIGHT) && real_ball_x < WINDOW_WIDTH) {
+            *ps_x += 10;
+        }
+
+        if (IsKeyDown(KEY_LEFT) && real_ball_x > (BALL_SIZE * 2)) {
+            *ps_x -= 10;
+        }
+
+        *accumulator -= FIXED_TIME_STEP;
+    }
+}
+
+int is_on_the_ground(float real_ball_y)
+{
+    if (real_ball_y == WINDOW_HEIGHT) return 1;  // on the ground
+    return 0;
+}
+
+void ball_jumb(float *ball_velocity)
+{
+    if (IsKeyPressed(KEY_SPACE)) {
+        *ball_velocity = -200;
+    }
 }
